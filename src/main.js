972131f1f57079cf68b313cbe661f3bbef5a736a
cww19909999 <394509136@qq.com>
2020-05-27 06:11:35 +0800

    编写登录页的居中布局

--- src/main.js
+++ src/main.js
@@ -2,8 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {
+  Container,
+  Header,
+  Aside,
+  Main,
+  Button
+} from 'element-ui'
+
 
 Vue.config.productionTip = false
+
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Button)
+
 
 new Vue({
   router,
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>登录</div>
+<div class="login-wrapper">
+<el-container>
+    <el-main>Main</el-main>
+</el-container>
+</div>
+
 </template>
 <script>
 export default {
@@ -7,5 +12,45 @@ export default {
 }
 </script>
 <style scoped>
-
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+  .login-wrapper{
+      position: absolute;
+      top: 50%;
+      /* bottom: 0; */
+      left: 50%;
+      /* right: 0; */
+      transform: translate(-50%, -50%);
+  }
 </style>
